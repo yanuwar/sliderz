4622e0ebf2ee6111a21c2e6d99a4d9e272127292
zanuwar <zanzahmad@gmail.com>
2018-07-22 19:47:24 +0700

    adding auto slide

--- sliderz/src/main/java/com/skyshi/slider/sliderz/ui/Sliderz.kt
+++ sliderz/src/main/java/com/skyshi/slider/sliderz/ui/Sliderz.kt
@@ -1,6 +1,5 @@
 package com.skyshi.slider.sliderz.ui
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
@@ -10,24 +9,72 @@ import android.support.design.widget.TabLayout
 import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
+import android.content.res.TypedArray
+import android.os.AsyncTask
+import java.util.*
 
-@SuppressLint("StaticFieldLeak")
+
 lateinit var view_pager: ViewPager
-@SuppressLint("StaticFieldLeak")
 lateinit var indicator: TabLayout
-@SuppressLint("StaticFieldLeak")
 lateinit var view: View
+lateinit var contextSlider: Context
+lateinit var adapterSlider: PagerAdapter
 
 class Sliderz(context: Context?, attributeSet: AttributeSet?) : LinearLayout(context, attributeSet) {
     init {
-        //Inflate view
+        contextSlider = context!!
+        val a = context.theme?.obtainStyledAttributes(
+                attributeSet,
+                R.styleable.Sliderz,
+                0, 0)
         view = LayoutInflater.from(getContext()).inflate(R.layout.slide_parent, this, true)
         view_pager = view.findViewById(R.id.view_pager)
         indicator = view.findViewById(R.id.indicator_bottom)
         indicator.setupWithViewPager(view_pager, false)
+
+        initViewStyle(a)
     }
 
     fun setAdapter(adapter: PagerAdapter) {
+        adapterSlider = adapter
         view_pager.adapter = adapter
+    }
+
+    private fun initViewStyle(a: TypedArray?) {
+        setVisibilityBottomIndicator(a!!.getBoolean(R.styleable.Sliderz_bottom_indicator_visibility, false))
+        isAutoSlide (true)
+    }
+
+    fun setVisibilityBottomIndicator(show: Boolean) {
+        if (show) indicator.visibility = View.VISIBLE else indicator.visibility = View.INVISIBLE
+    }
+
+    fun isAutoSlide (auto: Boolean) {
+        if (auto) {
+            val timer = Timer()
+            timer.scheduleAtFixedRate(SliderTimer(), 4000, 6000)
+        }
+    }
+
+    private inner class SliderTimer : TimerTask() {
+        override fun run() {
+            RunningTask().execute()
+        }
+    }
+
+    private inner class RunningTask() : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            return ""
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (view_pager.currentItem < adapterSlider.count - 1) {
+                view_pager.currentItem = view_pager.currentItem + 1
+            } else {
+                view_pager.currentItem = 0
+            }
+        }
+
     }
 }
